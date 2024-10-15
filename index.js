bab96d4387c9c236508ceb2dfda0a0a8bf964ff5
Emmanuel Kwame Plockey <emmaplockey123@gmail.com>
2024-10-15 11:37:16 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const multer = require("multer");
+const uploads = multer({dest: "uploads/"})
+require('dotenv').config()
+
+const app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post("/api/fileanalyze", uploads.single("upfile"), (req, res) => {
+  const {originalname, mimetype, size} = req.file;
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size
+  })
+  
+})
+
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
